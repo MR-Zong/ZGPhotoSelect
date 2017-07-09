0d605c135703660e28aaf70b3907c4586066e4ff
ZongGenXu <540682674@qq.com>
2017-07-09 10:26:50 +0800

    展示效果

--- ZGPhotoSelect/ZGPhotoSelect/ViewController.m
+++ ZGPhotoSelect/ZGPhotoSelect/ViewController.m
@@ -8,7 +8,46 @@
 
 #import "ViewController.h"
 #import "ZGPhotoPickupController.h"
+#import "UIImage+DHUtil.h"
 
+@interface MYQPhotoGraphFillColorView : UIView
+@property (nonatomic, strong) UIImageView *imageView;
+- (void)clipWithRect:(CGRect)rect;
+@end
+
+@implementation MYQPhotoGraphFillColorView
+
+- (instancetype)initWithFrame:(CGRect)frame
+{
+    if (self = [super initWithFrame:frame]) {
+        
+        _imageView = [[UIImageView alloc] initWithFrame:frame];
+        _imageView.image = [UIImage imageFromColor:[[UIColor whiteColor] colorWithAlphaComponent:0.3] size:frame.size];
+        [self addSubview:_imageView];
+    }
+    return self;
+}
+
+- (void)clipWithRect:(CGRect)rect
+{
+    UIGraphicsBeginImageContextWithOptions(self.frame.size, NO, 0);
+    CGContextRef context =  UIGraphicsGetCurrentContext();
+    // 把imageView的layer映射到上下文中
+    [self.imageView.layer renderInContext:context];
+    // 清除划过的区域
+    CGContextClearRect(context, rect);
+    // 获取图片
+    UIImage *image = UIGraphicsGetImageFromCurrentImageContext();
+    // 结束图片的画板, (意味着图片在上下文中消失)
+    UIGraphicsEndImageContext();
+    self.imageView.image = image;
+    
+}
+
+@end
+
+
+#pragma mark -
 @interface ViewController ()
 
 @property (nonatomic, strong) ZGPhotoPickupController *photoPickupVC;
@@ -33,11 +72,62 @@
 #pragma mark - exampleButtonClick
 - (void)exampleButtonClick:(UIButton *)btn
 {
+    [self example1];
+}
+
+- (void)example1
+{
     self.photoPickupVC = [ZGPhotoPickupController photoPickupWithViewController:self completeBlock:^(NSArray *imageArray, NSArray *assetArray, NSInteger code) {
         NSLog(@"image pickup success");
     }];
-  
+    
     [self.photoPickupVC show];
+
+}
+
+/**
+ * 该样例: 必须真机运行
+ */
+- (void)example2
+{
+    UIView *overlayView;
+    overlayView = [[UIView alloc] initWithFrame:CGRectMake(0, 40.0, [UIScreen mainScreen].bounds.size.width, [UIScreen mainScreen].bounds.size.height - 40.0 - 100.0)];
+    
+    
+    CGFloat idCardBorderImgViewWidth = 260.0;
+    CGFloat idCardBorderImgViewHeight = idCardBorderImgViewWidth / (8.0 / 5.0);
+    CGRect idCardBorderImgViewFrame = CGRectMake(([UIScreen mainScreen].bounds.size.width - idCardBorderImgViewWidth) / 2.0, ([UIScreen mainScreen].bounds.size.height - idCardBorderImgViewHeight) / 2.0 - 64.0, idCardBorderImgViewWidth, idCardBorderImgViewHeight);
+    
+    MYQPhotoGraphFillColorView *bgColorView = [[MYQPhotoGraphFillColorView alloc] initWithFrame:overlayView.bounds];
+    [bgColorView clipWithRect:idCardBorderImgViewFrame];
+    [overlayView addSubview:bgColorView];
+    
+    
+    UIImageView *idCardBorderImgView = [[UIImageView alloc] init];
+    UIImage *img = [UIImage imageNamed:@"id_finder"];
+    UIEdgeInsets edgeInset = UIEdgeInsetsMake(20, 20, 20, 20);
+    idCardBorderImgView.image = [img resizableImageWithCapInsets:edgeInset resizingMode:UIImageResizingModeStretch];
+    idCardBorderImgView.frame = idCardBorderImgViewFrame;
+    [overlayView addSubview:idCardBorderImgView];
+    
+    CGFloat tipLabelWidth = 200.0;
+    CGFloat tipLabelHeight = 20.0;
+    UILabel *tipLabel = [[UILabel alloc] initWithFrame:CGRectMake(([UIScreen mainScreen].bounds.size.width - tipLabelWidth) / 2.0, CGRectGetMaxY(idCardBorderImgView.frame) + 5.0, tipLabelWidth, tipLabelHeight)];
+    tipLabel.text = @"请将卡片边缘对齐方框以便拍照";
+    tipLabel.font = [UIFont systemFontOfSize:14];
+    tipLabel.textColor = [UIColor whiteColor];
+    tipLabel.textAlignment = NSTextAlignmentCenter;
+    [overlayView addSubview:tipLabel];
+
+
+    
+    self.photoPickupVC = [ZGPhotoPickupController photoPickupWithViewController:self completeBlock:^(NSArray *imageArray, NSArray *assetArray, NSInteger code) {
+        NSLog(@"image pickup success");
+    }];
+    
+    self.photoPickupVC.photoGraphVC.cameraOverlayView = overlayView;
+    [self.photoPickupVC show];
+    
 }
 
 
