dbb1e340c221b34518c255c8b1e56d3552df972e
ZongGenXu <540682674@qq.com>
2017-07-16 17:10:52 +0800

    拍照效果优化

--- ZGPhotoSelect/ZGPhotoSelect/ViewController.m
+++ ZGPhotoSelect/ZGPhotoSelect/ViewController.m
@@ -13,6 +13,8 @@
 @interface MYQPhotoGraphFillColorView : UIView
 @property (nonatomic, strong) UIImageView *imageView;
 - (void)clipWithRect:(CGRect)rect;
+- (void)setMaskLayerWithView:(UIView *)view maskRect:(CGRect)rect cornerRadius:(CGFloat)cornerRadius;
+- (void)bezierMaskLayerWithView:(UIView *)view maskRect:(CGRect)rect cornerRadius:(CGFloat)cornerRadius;
 @end
 
 @implementation MYQPhotoGraphFillColorView
@@ -44,6 +46,67 @@
     
 }
 
+
+// 贝塞尔path
+- (void)bezierMaskLayerWithView:(UIView *)view maskRect:(CGRect)rect cornerRadius:(CGFloat)cornerRadius
+{
+    //贝塞尔曲线 画一个带矩形
+    UIBezierPath *bpath = [UIBezierPath bezierPathWithRect:view.bounds];
+    //贝塞尔曲线 画一个带有圆角的矩形
+    [bpath appendPath: [[UIBezierPath bezierPathWithRoundedRect:rect cornerRadius:cornerRadius] bezierPathByReversingPath]];
+    
+    //创建一个CAShapeLayer 图层
+    CAShapeLayer *shapeLayer = [CAShapeLayer layer];
+    shapeLayer.path = bpath.CGPath;
+    view.layer.mask = shapeLayer;
+}
+
+//绘制裁剪区分图层
+- (void)setMaskLayerWithView:(UIView *)view maskRect:(CGRect)rect cornerRadius:(CGFloat)cornerRadius
+{
+    if(cornerRadius == 0)
+    {
+        return;
+    }
+    
+    CAShapeLayer *maskLayer = [[CAShapeLayer alloc] init];
+    
+    CGMutablePathRef mainPath = CGPathCreateMutable();
+    CGPathAddRect(mainPath, NULL, rect);
+    
+    
+    CGMutablePathRef subPath = CGPathCreateMutable();
+    
+    CGFloat minx = CGRectGetMinX(rect);
+    CGFloat midx = CGRectGetMidX(rect);
+    CGFloat maxx = CGRectGetMaxX(rect);
+    
+    CGFloat miny = CGRectGetMinY(rect);
+    CGFloat midy = CGRectGetMidY(rect);
+    CGFloat maxy = CGRectGetMaxY(rect);
+    
+    
+    CGPathMoveToPoint(subPath, NULL, minx, midy);
+    
+    CGPathAddArcToPoint(subPath, NULL,minx, miny, midx, miny, cornerRadius);
+    
+    CGPathAddArcToPoint(subPath, NULL,maxx, miny, maxx, midy, cornerRadius);
+    
+    CGPathAddArcToPoint(subPath, NULL, maxx, maxy, midx, maxy, cornerRadius);
+    
+    CGPathAddArcToPoint(subPath, NULL,minx, maxy, minx, midy, cornerRadius);
+    
+    
+    CGPathAddPath(mainPath, NULL, subPath);
+    
+    
+    maskLayer.path = mainPath;
+    view.layer.mask = maskLayer;
+    CGPathRelease(subPath);
+    CGPathRelease(mainPath);
+}
+
+
 @end
 
 
@@ -72,7 +135,7 @@
 #pragma mark - exampleButtonClick
 - (void)exampleButtonClick:(UIButton *)btn
 {
-    [self example1];
+    [self example2];
 }
 
 - (void)example1
@@ -99,7 +162,9 @@
     CGRect idCardBorderImgViewFrame = CGRectMake(([UIScreen mainScreen].bounds.size.width - idCardBorderImgViewWidth) / 2.0, ([UIScreen mainScreen].bounds.size.height - idCardBorderImgViewHeight) / 2.0 - 64.0, idCardBorderImgViewWidth, idCardBorderImgViewHeight);
     
     MYQPhotoGraphFillColorView *bgColorView = [[MYQPhotoGraphFillColorView alloc] initWithFrame:overlayView.bounds];
-    [bgColorView clipWithRect:idCardBorderImgViewFrame];
+    //        [bgColorView clipWithRect:idCardBorderImgViewFrame];
+    //        [bgColorView setMaskLayerWithView:bgColorView maskRect:idCardBorderImgViewFrame cornerRadius:15.f];
+    [bgColorView bezierMaskLayerWithView:bgColorView maskRect:idCardBorderImgViewFrame cornerRadius:15.f];
     [overlayView addSubview:bgColorView];
     
     
